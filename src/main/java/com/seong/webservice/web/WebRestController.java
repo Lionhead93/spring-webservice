413ba4e145a98701a14f80d5674c7fbe756a0167
Lionhead93 <lionhead115@naver.com>
2020-01-20 11:33:12 +0900

    nginx 무중단 배포

--- src/main/java/com/seong/webservice/web/WebRestController.java
+++ src/main/java/com/seong/webservice/web/WebRestController.java
@@ -4,10 +4,13 @@ import com.seong.webservice.dto.PostsSaveReqDto;
 import com.seong.webservice.repo.PostsRepository;
 import com.seong.webservice.service.PostsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +18,13 @@ public class WebRestController {
 
 
     private final PostsService postsService;
+    private final Environment environment;
 
-    @GetMapping("/hello")
+    @GetMapping("/profile")
     public String hello() {
-        return "HelloWorld";
+        return Arrays.stream(environment.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 
     @PostMapping("/posts")
